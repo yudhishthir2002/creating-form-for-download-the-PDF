effb846347ace26f98b06f9894135b7dfda1094f
yudhishthir2002 <yudhishthirsharma2003@gmail.com>
2025-05-20 15:32:21 +0530

    change the form hight

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import DownloadForm from './component/DownloadForm';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div className="h-full bg-gray-100 flex items-center justify-center">
       <DownloadForm />
     </div>
   );
