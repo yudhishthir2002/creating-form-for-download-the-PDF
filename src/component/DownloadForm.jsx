283c841cb55abef7ad0a2f43618fb095af9e6b25
yudhishthir2002 <yudhishthirsharma2003@gmail.com>
2025-05-21 16:40:44 +0530

    add state

--- src/component/DownloadForm.jsx
+++ src/component/DownloadForm.jsx
@@ -11,6 +11,7 @@ const DownloadForm = () => {
   });
 
   const [errors, setErrors] = useState({});
+  const [isOpen, setisOpen] = useState(false);
 
   const validate = () => {
     const newErrors = {};
@@ -71,85 +72,87 @@ const DownloadForm = () => {
   };
 
   return (
-    <div className="w-full max-w-lg mx-auto p-6 bg-white rounded-xl shadow-md mt-10">
-      <h2 className="text-3xl font-medium text-center mb-6 text-blue-800 ">
-        Get Free Consultation!
-      </h2>
-      <form onSubmit={handleSubmit} className="space-y-4">
-        {/* Name */}
-        <div>
-          <label className="block text-gray-700 font-medium mb-1">
-            Name <span className="text-red-500"> *</span>
-          </label>
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring focus:border-blue-500"
-          />
-          {errors.name && (
-            <p className="text-red-600 text-sm mt-1">{errors.name}</p>
-          )}
-        </div>
-
-        {/* Email */}
-        <div>
-          <label className="block text-gray-700 font-medium mb-1">
-            Email <span className="text-red-500"> *</span>
-          </label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring focus:border-blue-500"
-          />
-          {errors.email && (
-            <p className="text-red-600 text-sm mt-1">{errors.email}</p>
-          )}
-        </div>
-
-        {/* Contact Number */}
-        <div>
-          <label className="block text-gray-700 font-medium mb-1">
-            Contact Number <span className="text-red-500"> *</span>
-          </label>
-          <input
-            type="tel"
-            name="contact"
-            value={formData.contact}
-            onChange={handleChange}
-            className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring focus:border-blue-500"
-          />
-          {errors.contact && (
-            <p className="text-red-600 text-sm mt-1">{errors.contact}</p>
-          )}
-        </div>
-
-        {/* Message */}
-        <div>
-          <label className="block text-gray-700 font-medium mb-1">
-                      Message
-                     
-          </label>
-          <textarea
-            name="message"
-            rows="4"
-            value={formData.message}
-            onChange={handleChange}
-            className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring focus:border-blue-500 resize-none"
-          />
+    <>
+    
+      <div className="w-full max-w-lg mx-auto p-6 bg-white rounded-xl shadow-md mt-10">
+        <h2 className="text-3xl font-medium text-center mb-6 text-blue-800 ">
+          Get Free Consultation!
+        </h2>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          {/* Name */}
+          <div>
+            <label className="block text-gray-700 font-medium mb-1">
+              Name <span className="text-red-500"> *</span>
+            </label>
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring focus:border-blue-500"
+            />
+            {errors.name && (
+              <p className="text-red-600 text-sm mt-1">{errors.name}</p>
+            )}
           </div>
 
-        <button
-          type="submit"
-          className="w-full bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg transition duration-200"
-        >
-          Submit & Download PDF
-        </button>
-      </form>
-    </div>
+          {/* Email */}
+          <div>
+            <label className="block text-gray-700 font-medium mb-1">
+              Email <span className="text-red-500"> *</span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring focus:border-blue-500"
+            />
+            {errors.email && (
+              <p className="text-red-600 text-sm mt-1">{errors.email}</p>
+            )}
+          </div>
+
+          {/* Contact Number */}
+          <div>
+            <label className="block text-gray-700 font-medium mb-1">
+              Contact Number <span className="text-red-500"> *</span>
+            </label>
+            <input
+              type="tel"
+              name="contact"
+              value={formData.contact}
+              onChange={handleChange}
+              className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring focus:border-blue-500"
+            />
+            {errors.contact && (
+              <p className="text-red-600 text-sm mt-1">{errors.contact}</p>
+            )}
+          </div>
+
+          {/* Message */}
+          <div>
+            <label className="block text-gray-700 font-medium mb-1">
+              Message
+            </label>
+            <textarea
+              name="message"
+              rows="4"
+              value={formData.message}
+              onChange={handleChange}
+              className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring focus:border-blue-500 resize-none"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg transition duration-200"
+          >
+            Submit & Download PDF
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
